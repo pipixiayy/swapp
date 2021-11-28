7930aaf738d3c8e0810331a81f99b04ebe5116df
yh.swag <jkjktv8@163.com>
2021-11-28 18:15:27 +0800

    2021_yh test

---
+++ src/main/java/com/yh/swaggerpro/controller/JunitTest.java
@@ -0,0 +1,23 @@
+package com.yh.swaggerpro.controller;
+
+/*import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+*//**
+ * @program: swagger-pro
+ * @Date: 2021/6/27 21:39
+ * @Author: YH
+ * @Description:
+ *//*
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JunitTest {
+
+    @Test
+    public void findAll() throws Exception {
+        System.out.println("ceshilei");
+    }*/
+public class JunitTest {
+}
